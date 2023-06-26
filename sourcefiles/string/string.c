4c5ccd6f0d90d0a31946fdb05d609de6145d9339
gumball <dariltonmario14@gmail.com>
2023-06-26 19:36:39 +0100

    Add functions to append a char to a string and to tell the size of one

--- sourcefiles/string/string.c
+++ sourcefiles/string/string.c
@@ -1,5 +1,8 @@
 #include "string.h"
 
+#define SPACE_FOR_END_OF_STRING 1
+#define SPACE_FOR_NEW_CHAR 1
+
 char *ler_palavra(FILE *fluxo_de_dados){
 	char *palavra;
 
@@ -24,3 +27,33 @@ char *ler_palavra(FILE *fluxo_de_dados){
 	return palavra;
 }
 
+int tamanho_da_palavra(char *palavra){
+	int tamanho = 0;
+	if(palavra == NULL)
+		return tamanho;
+
+	while(palavra[tamanho++] != '\0')
+		;
+
+	return tamanho - 1;
+}
+
+char *add_char_na_palavra(char *palavra, char char_para_adicionar){
+	if(palavra == NULL)
+		return NULL;
+
+	char *nova_palavra = malloc(tamanho_da_palavra(palavra) + SPACE_FOR_END_OF_STRING + SPACE_FOR_NEW_CHAR);
+
+	//copia palavra em nova_palavra
+	int i = 0;
+	while(palavra[i]){
+		nova_palavra[i] = palavra[i];
+		i++;
+	}
+
+	//addiciona o novo character e marca o fim da string
+	nova_palavra[i++] = char_para_adicionar;
+	nova_palavra[i] = '\0';
+
+	return nova_palavra;
+}
--- sourcefiles/string/string.h
+++ sourcefiles/string/string.h
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int tamanho_da_palavra(char *palavra);
+
 char *ler_palavra(FILE *fluxo_de_dados);
+
+char *add_char_na_palavra(char *palavra, char char_para_adicionar);
 
 #endif
