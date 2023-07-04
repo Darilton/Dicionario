70f3034e7cb51cb4144a0ba62e4e825b6b8f6eb2
gumball <dariltonmario14@gmail.com>
2023-07-04 20:50:34 +0100

    Add new funcionality to read words from a file

--- dicionario.c
+++ dicionario.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #if defined(_WIN32) || defined(_WIN64)
-#	include "sourcefiles\\string\\string.h"
+#	include "sourcefiles\\palavra\\palavra.h"
 #	include "sourcefiles\\trie\\trie.h"
 #	include "sourcefiles\\menu_utils\\menu_utils.h"
 #	define LIMPAR_TELA system("cls")
 #	define DICIONARIO "resources\\dicionario.dat"
 #else
-#	include "sourcefiles/string/string.h"
+#	include "sourcefiles/palavra/palavra.h"
 #	include "sourcefiles/trie/trie.h"
 #	include "sourcefiles/menu_utils/menu_utils.h"
 #	define LIMPAR_TELA system("clear")
@@ -69,7 +70,7 @@ int main(){
 				break;
 			case 3:
 				//ler palavra
-				printf("Eliminar Palavra\n\n");
+				printf("ELIMINAR PALAVRA\n\n");
 				printf("Insira a palavra que deseja eliminar\n");
 				printf("Palavra: ");
 				palavra = ler_palavra(stdin);
@@ -78,6 +79,24 @@ int main(){
 
 				break;
 			case 4:
+				//ler palavra antiga
+				printf("ATUALIZAR PALAVRA\n\n");
+				printf("Insira a palavra que deseja atualizar\n");
+				printf("Palavra antiga: ");
+				palavra = ler_palavra(stdin);
+
+				//ler nova palavra
+				printf("Insira a palavra atualizada\n");
+				printf("Palavra atualizada: ");
+				char *palavra_nova = ler_palavra(stdin);
+
+				switch(trie_atualizar_palavra(arvore, palavra, palavra_nova)){
+					case 0: printf("%s: palavra nao existe na arvore! Nada a fazer\n", palavra);
+					case 1: printf("%s: palavra atualizada para %s\n", palavra, palavra_nova);
+					case 2: printf("%s: palavra ja existe na arvore! Nada a fazer\n", palavra_nova);
+				}
+
+				free(palavra_nova);
 				break;
 			case 5:
 				//ler prefixo
@@ -98,20 +117,36 @@ int main(){
 				break;
 			case 7:
 				break;
-			case 8:/*
+			case 8:
 				//Verificador Ortografico
 				printf("\nVERIFICADOR ORTOGRAFICO\n");
-				printf("por favor insira o caminho para o ficheiro\n");
-				char caminho[] = scanf("%s", palavra);
-				printf("%s :caminho ", palavra);
+				printf("por favor insira o caminho para o ficheiro\nNo windows nao se esqueca de inserir duas barras\nEx: ..\\\\pasta\\\\ficheiro.dat\n");
+				char caminho[50], resposta;
+				int palavras_existem_no_dicionario = 1;		//flag que diz se todas as palavras do dicionario existem no ficheiro
+
+				//le o caminho
+				printf("ficheiro: ");
+				fgets(caminho, 50, stdin);
+
+				//remove o \n lido da variavel
+				size_t ln = strlen(caminho) - 1;
+				if (*caminho && caminho[ln] == '\n')
+					caminho[ln] = '\0';	
+
+				printf("caminho: %s\n", caminho);
 				
-				FILE *ficheiro = fopen("r", caminho_para_o_ficheiro);
+				
+				FILE *ficheiro = fopen(caminho, "r+");
+
+				if(ficheiro == NULL){
+					printf("Nao foi possivel abrir o ficheiro especificado\n");
+					break;
+				}
+
 				while(palavra = ler_palavra(ficheiro)){
 					//se palavra nao existe
 					if(!trie_procurar_palavra(arvore,palavra)){
-						//Sugere palavra similar
-							//sugerir palavra similar
-
+						palavras_existem_no_dicionario = 0;
 						//Sugere insercao no dicionario
 						printf("%s nao existe no dicionario! Deseja adiciona-la?(s/n): ", palavra);
 						scanf("%c", &resposta);
@@ -120,13 +155,21 @@ int main(){
 						if((resposta == 's') || (resposta == 'S')){
 							trie_inserir_palavra(arvore,palavra);
 							printf("%s: palavra inserida com sucesso\n", palavra);
-						}
-					}
+						}else{
+							long num_carac_a_voltar = - sizeof(char);
 
+							fseek(ficheiro, num_carac_a_voltar, SEEK_CUR);
+							fprintf(ficheiro,"*");
+						}
+						getchar();
+					}
 					free(palavra);
 				}
-				*/
 
+				if(palavras_existem_no_dicionario)
+					printf("Todas as palavras deste ficheiro existem no dicionario!\nNada a fazer\n");
+
+				fclose(ficheiro);
 				break;
 			case 9:
 				//imprimir arvore
